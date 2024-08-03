281fc7741c22b0e5fcfb1fac1fb6c6dd1e4f6e79
Evgeniy <Baldin.128500@gmail.com>
2024-08-03 19:55:50 +0300

    Change return type

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,8 +2,7 @@ public class BmiService {
     public double calculate(double heightMetre, double weightKg) {
         double heightSquare = heightMetre * heightMetre;
         double bmiresult = weightKg / heightSquare;
-        int result = (int) bmiresult;
-        return result;
+        return (int) bmiresult;
     }
 }
 
