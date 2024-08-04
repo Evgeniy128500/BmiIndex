8d045f0e7e7773a43dc6c069f1220fa4fd07c8fd
Evgeniy <Baldin.128500@gmail.com>
2024-08-04 18:42:23 +0300

    Modified typ

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,7 +2,7 @@ public class BmiService {
     public double calculate(double heightMetre, double weightKg) {
         double heightSquare = heightMetre * heightMetre;
         double bmiresult = weightKg / heightSquare;
-        return (int) bmiresult;
+        return  bmiresult;
     }
 }
 
